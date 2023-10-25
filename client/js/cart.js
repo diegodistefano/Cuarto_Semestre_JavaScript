c6c97f3a21735d02b4ead41e8044cf9f6c6c2052
diegodistefano <diego.distefano.1989@gmail.com>
2023-10-24 21:35:42 -0300

    Clase 2. Header

--- client/js/cart.js
+++ client/js/cart.js
@@ -4,8 +4,9 @@ const modalOverlay = document.getElementById("modal-overlay");
 const cartBtn = document.getElementById("cart-btn");
 
 const displayCart = () => {
-
-    
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "block";
+    modalOverlay.style.display = "block";
     //Modal Header
     const modalHeader = document.createElement("div");
 
@@ -13,6 +14,11 @@ const displayCart = () => {
     modalClose.innerText = "❌​";
     modalClose.className = "modal-close";
     modalHeader.append(modalClose);
+
+    modalClose.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+        modalOverlay.style.display = "none";
+    });
 
     const modalTitle = document.createElement("div");
     modalTitle.innerText = "Cart";
